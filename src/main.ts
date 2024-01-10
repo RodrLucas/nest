78484a62e9eb37cef8c2106e0f328a4ca3acc517
RodrLucas <lucasrodriguessantos0@gmail.com>
2024-01-10 11:25:23 -0300

    feat: adding users and places

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,12 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ValidateInputPipe } from './core/pipes/validate.pipe';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  app.setGlobalPrefix('api/v1');
+  const port = process.env.PORT || 3001;
+  app.useGlobalPipes(new ValidateInputPipe());
+  await app.listen(port);
 }
 bootstrap();
